e244a1368c411aee9cc5c0a9537bdbe3202ab501
Mario Zarate <m.zaratev@gmail.com>
2021-01-11 20:00:45 -0500

    Pass first test

--- tests/MobileTest.php
+++ tests/MobileTest.php
@@ -4,6 +4,8 @@ namespace Tests;
 
 use Mockery as m;
 use PHPUnit\Framework\TestCase;
+use App\Mobile;
+use App\Interfaces\CarrierInterface;
 
 class MobileTest extends TestCase
 {
@@ -11,6 +13,9 @@ class MobileTest extends TestCase
 	/** @test */
 	public function it_returns_null_when_name_empty()
 	{
+        $provider = $this->getMockBuilder(CarrierInterface::class)
+                ->setMockClassName('NationalCarrier')
+                ->getMock();
 		$mobile = new Mobile($provider);
 
 		$this->assertNull($mobile->makeCallByName(''));
