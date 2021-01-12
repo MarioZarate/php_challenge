a890e932911f4a4d7bcf552f7b6140956ebb1940
Mario Zarate <m.zaratev@gmail.com>
2021-01-11 21:11:14 -0500

    Add test for makeCallByName

--- tests/MobileTest.php
+++ tests/MobileTest.php
@@ -2,10 +2,13 @@
 
 namespace Tests;
 
+use App\Call;
+use App\Contact;
 use Mockery as m;
 use PHPUnit\Framework\TestCase;
 use App\Mobile;
 use App\Interfaces\CarrierInterface;
+use App\Services\ContactService;
 
 class MobileTest extends TestCase
 {
@@ -19,6 +22,22 @@ class MobileTest extends TestCase
 		$mobile = new Mobile($provider);
 
 		$this->assertNull($mobile->makeCallByName(''));
-	}
+    }
+    
+    public function test_makeCallByName() 
+    {
+        $contactServiceMock = m::mock('alias:App\Services\ContactService');
+        $contactServiceMock
+            ->shouldReceive('findByName')
+            ->andReturn(new Contact());
+        
+        $provider = $this->getMockBuilder(CarrierInterface::class)
+                ->setMockClassName('NationalCarrier')
+                ->getMock();
+        $provider->method('dialContact')->willReturn(true);
+        $provider->method('makeCall')->willReturn(new Call());
+        $mobile = new Mobile($provider);
+        $this->assertInstanceOf(Call::class, $mobile->makeCallByName('TestUser'));
+    }
 
 }
